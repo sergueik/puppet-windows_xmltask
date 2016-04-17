6bd6d384c2b8ecedf639c83a44906fa01b40154e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-04-17 17:54:17 -0400

    intermediate revision -  described idempotency problem

--- manifests/job_definition.pp
+++ manifests/job_definition.pp
@@ -18,6 +18,26 @@ define windows_xmltask::job_definition(
   validate_string($domain)
   validate_re($version, '^\d+\.\d+\.\d+(-\d+)*$')
   $taskname = regsubst($title, '[$/\\|:, ]', '_', 'G')
+  # NOTE: use backslashes in generate tool path 
+  $task_year = regsubst(regsubst(generate('c:\windows\system32\cmd.exe', '/c date /t'), '^.+/', ''), '\n', '')
+  # NOTE: scheduled_tast idempotency issue -  trigger/start_date accepts only MM/DD/yyyy but returns YYYY-MM-DD
+  scheduled_task {
+    ensure => present,
+    enabled => true,
+    command => 'c:/windows/system32/windowspowershell/v1.0/powershell.exe',
+    arguments => c:/windows/temp/sample.ps1',,
+    trigged => {
+      schedule => weekely,
+      day_of_week => 'mon',
+      start_time => '23:00:00',
+      start_date => "01/01/${task_year}"
+      # will report trigger changed 
+      # {'every' => '1', 'on' => ['mon'], 'schedule' => 'weekely', 'start_date' => '2016-1-1', 'start_time' => '23:0:00' }
+      # to
+      # ['day_of_week' => 'mon', 'schedule' => 'weekly' , 'start_date' => '01/01/2016', start_time => '23:00:00']
+    }
+  } 
+  
   # generate job definition from template
   file { "c:\\windows\\temp\\${script}.xml":
     ensure             => file,
